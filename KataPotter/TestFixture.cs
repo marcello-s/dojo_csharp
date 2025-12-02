97d86a158e92f66ce628631837839cd8d2bcc63d
marcello-s <marcello.s@gmx.ch>
2025-12-02 16:37:23 +0100

    revise asserts

--- KataPotter/TestFixture.cs
+++ KataPotter/TestFixture.cs
@@ -35,24 +35,24 @@ namespace KataPotter
         [Test]
         public void TestItemTotal()
         {
-            basket.AddItem(new Item("book1", 8 ));
-            basket.AddItem(new Item("book2", 8 ));
+            basket.AddItem(new Item("book1", 8));
+            basket.AddItem(new Item("book2", 8));
             grandTotal.Calculate();
-            Assert.That(16, Is.EqualTo(grandTotal.ItemTotal));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(16));
         }
 
         [Test]
         public void TestNoDiscount2Items()
         {
-            basket.AddItem(new Item("book1", 8 ));
-            basket.AddItem(new Item("book1",8 ));
+            basket.AddItem(new Item("book1", 8));
+            basket.AddItem(new Item("book1", 8));
             var discount = new Discount("5% per unique item");
             discount.Percentage = 0.05M;
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 2, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 * 2, Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 2));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 * 2));
         }
 
         [Test]
@@ -66,8 +66,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 3, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 * 3, Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 3));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 * 3));
         }
 
         [Test]
@@ -80,8 +80,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 2, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 * 2 * 0.95M, Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 2));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 * 2 * 0.95M));
         }
 
         [Test]
@@ -95,8 +95,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 3, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 * 3 * 0.9M, Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 3));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 * 3 * 0.9M));
         }
 
         [Test]
@@ -111,8 +111,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 4, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 * 4 * 0.8M, Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 4));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 * 4 * 0.8M));
         }
 
         [Test]
@@ -128,8 +128,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 5, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 * 5 * 0.75M, Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 5));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 * 5 * 0.75M));
         }
 
         [Test]
@@ -143,8 +143,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 3, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 + (8 * 2 * 0.95M), Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 3));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 + (8 * 2 * 0.95M)));
         }
 
         [Test]
@@ -159,8 +159,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 4, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(2 * (8 * 2 * 0.95M), Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 4));
+            Assert.That(grandTotal.Total, Is.EqualTo(2 * (8 * 2 * 0.95M)));
         }
 
         [Test]
@@ -177,8 +177,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 6, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That((8 * 4 * 0.8M) + (8 * 2 * 0.95M), Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 6));
+            Assert.That(grandTotal.Total, Is.EqualTo((8 * 4 * 0.8M) + (8 * 2 * 0.95M)));
         }
 
         [Test]
@@ -195,8 +195,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 6, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(8 + (8 * 5 * 0.75M), Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 6));
+            Assert.That(grandTotal.Total, Is.EqualTo(8 + (8 * 5 * 0.75M)));
         }
 
         [Test]
@@ -215,8 +215,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 8, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(2 * (8 * 4 * 0.8M), Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 8));
+            Assert.That(grandTotal.Total, Is.EqualTo(2 * (8 * 4 * 0.8M)));
         }
 
         [Test]
@@ -250,8 +250,8 @@ namespace KataPotter
             discount.Basket = basket;
             discounts.Add(discount);
             grandTotal.Calculate();
-            Assert.That(8 * 23, Is.EqualTo(grandTotal.ItemTotal));
-            Assert.That(3 * (8 * 5 * 0.75M) + 2 * (8 * 4 * 0.8M), Is.EqualTo(grandTotal.Total));
+            Assert.That(grandTotal.ItemTotal, Is.EqualTo(8 * 23));
+            Assert.That(grandTotal.Total, Is.EqualTo(3 * (8 * 5 * 0.75M) + 2 * (8 * 4 * 0.8M)));
         }
     }
 }
--- KataRomanNumerals/TestFixture.cs
+++ KataRomanNumerals/TestFixture.cs
@@ -15,207 +15,207 @@ namespace KataRomanNumerals
         [Test]
         public void TestI()
         {
-            Assert.That("I", Is.EqualTo(RomanConverter.Convert("1")));
+            Assert.That(RomanConverter.Convert("1"), Is.EqualTo("I"));
         }
 
         [Test]
         public void TestII()
         {
-            Assert.That("II", Is.EqualTo(RomanConverter.Convert("2")));
+            Assert.That(RomanConverter.Convert("2"), Is.EqualTo("II"));
         }
 
         [Test]
         public void TestIII()
         {
-            Assert.That("III", Is.EqualTo(RomanConverter.Convert("3")));
+            Assert.That(RomanConverter.Convert("3"), Is.EqualTo("III"));
         }
 
         [Test]
         public void TestIV()
         {
-            Assert.That("IV", Is.EqualTo(RomanConverter.Convert("4")));
+            Assert.That(RomanConverter.Convert("4"), Is.EqualTo("IV"));
         }
 
         [Test]
         public void TestV()
         {
-            Assert.That("V", Is.EqualTo(RomanConverter.Convert("5")));
+            Assert.That(RomanConverter.Convert("5"), Is.EqualTo("V"));
         }
 
         [Test]
         public void TestVI()
         {
-            Assert.That("VI", Is.EqualTo(RomanConverter.Convert("6")));
+            Assert.That(RomanConverter.Convert("6"), Is.EqualTo("VI"));
         }
 
         [Test]
         public void TestVII()
         {
-            Assert.That("VII", Is.EqualTo(RomanConverter.Convert("7")));
+            Assert.That(RomanConverter.Convert("7"), Is.EqualTo("VII"));
         }
 
         [Test]
         public void TestVIII()
         {
-            Assert.That("VIII", Is.EqualTo(RomanConverter.Convert("8")));
+            Assert.That(RomanConverter.Convert("8"), Is.EqualTo("VIII"));
         }
 
         [Test]
         public void TestIX()
         {
-            Assert.That("IX", Is.EqualTo(RomanConverter.Convert("9")));
+            Assert.That(RomanConverter.Convert("9"), Is.EqualTo("IX"));
         }
 
         [Test]
         public void TestX()
         {
-            Assert.That("X", Is.EqualTo(RomanConverter.Convert("10")));
+            Assert.That(RomanConverter.Convert("10"), Is.EqualTo("X"));
         }
 
         [Test]
         public void TestL()
         {
-            Assert.That("L", Is.EqualTo(RomanConverter.Convert("50")));
+            Assert.That(RomanConverter.Convert("50"), Is.EqualTo("L"));
         }
 
         [Test]
         public void TestC()
         {
-            Assert.That("C", Is.EqualTo(RomanConverter.Convert("100")));
+            Assert.That(RomanConverter.Convert("100"), Is.EqualTo("C"));
         }
 
         [Test]
         public void TestD()
         {
-            Assert.That("D", Is.EqualTo(RomanConverter.Convert("500")));
+            Assert.That(RomanConverter.Convert("500"), Is.EqualTo("D"));
         }
 
         [Test]
         public void TestM()
         {
-            Assert.That("M", Is.EqualTo(RomanConverter.Convert("1000")));
+            Assert.That(RomanConverter.Convert("1000"), Is.EqualTo("M"));
         }
 
         [Test]
         public void TestMCMXCIX()
         {
-            Assert.That("MCMXCIX", Is.EqualTo(RomanConverter.Convert("1999")));
+            Assert.That(RomanConverter.Convert("1999"), Is.EqualTo("MCMXCIX"));
         }
 
         [Test]
         public void TestMMVIII()
         {
-            Assert.That("MMVIII", Is.EqualTo(RomanConverter.Convert("2008")));
+            Assert.That(RomanConverter.Convert("2008"), Is.EqualTo("MMVIII"));
         }
 
         /*
         [Test]
         public void TestMMMMMMMMMM()
         {
-            Assert.That("MMMMMMMMMM", Is.EqualTo(RomanConverter.Convert("10000")));
+            Assert.That(RomanConverter.Convert("10000"), Is.EqualTo("MMMMMMMMMM"));
         }
         */
 
         [Test]
         public void Test1()
         {
-            Assert.That("1", Is.EqualTo(RomanConverter.Convert("I")));
+            Assert.That(RomanConverter.Convert("I"), Is.EqualTo("1"));
         }
 
         [Test]
         public void Test2()
         {
-            Assert.That("2", Is.EqualTo(RomanConverter.Convert("II")));
+            Assert.That(RomanConverter.Convert("II"), Is.EqualTo("2"));
         }
 
         [Test]
         public void Test3()
         {
-            Assert.That("3", Is.EqualTo(RomanConverter.Convert("III")));
+            Assert.That(RomanConverter.Convert("III"), Is.EqualTo("3"));
         }
 
         [Test]
         public void Test4()
         {
-            Assert.That("4", Is.EqualTo(RomanConverter.Convert("IV")));
+            Assert.That(RomanConverter.Convert("IV"), Is.EqualTo("4"));
         }
 
         [Test]
         public void Test5()
         {
-            Assert.That("5", Is.EqualTo(RomanConverter.Convert("V")));
+            Assert.That(RomanConverter.Convert("V"), Is.EqualTo("5"));
         }
 
         [Test]
         public void Test6()
         {
-            Assert.That("6", Is.EqualTo(RomanConverter.Convert("VI")));
+            Assert.That(RomanConverter.Convert("VI"), Is.EqualTo("6"));
         }
 
         [Test]
         public void Test7()
         {
-            Assert.That("7", Is.EqualTo(RomanConverter.Convert("VII")));
+            Assert.That(RomanConverter.Convert("VII"), Is.EqualTo("7"));
         }
 
         [Test]
         public void Test8()
         {
-            Assert.That("8", Is.EqualTo(RomanConverter.Convert("VIII")));
+            Assert.That(RomanConverter.Convert("VIII"), Is.EqualTo("8"));
         }
 
         [Test]
         public void Test9()
         {
-            Assert.That("9", Is.EqualTo(RomanConverter.Convert("IX")));
+            Assert.That(RomanConverter.Convert("IX"), Is.EqualTo("9"));
         }
 
         [Test]
         public void Test10()
         {
-            Assert.That("10", Is.EqualTo(RomanConverter.Convert("X")));
+            Assert.That(RomanConverter.Convert("X"), Is.EqualTo("10"));
         }
 
         [Test]
         public void Test50()
         {
-            Assert.That("50", Is.EqualTo(RomanConverter.Convert("L")));
+            Assert.That(RomanConverter.Convert("L"), Is.EqualTo("50"));
         }
 
         [Test]
         public void Test100()
         {
-            Assert.That("100", Is.EqualTo(RomanConverter.Convert("C")));
+            Assert.That(RomanConverter.Convert("C"), Is.EqualTo("100"));
         }
 
         [Test]
         public void Test500()
         {
-            Assert.That("500", Is.EqualTo(RomanConverter.Convert("D")));
+            Assert.That(RomanConverter.Convert("D"), Is.EqualTo("500"));
         }
 
         [Test]
         public void Test1000()
         {
-            Assert.That("1000", Is.EqualTo(RomanConverter.Convert("M")));
+            Assert.That(RomanConverter.Convert("M"), Is.EqualTo("1000"));
         }
 
         [Test]
         public void Test1999()
         {
-            Assert.That("1999", Is.EqualTo(RomanConverter.Convert("MCMXCIX")));
+            Assert.That(RomanConverter.Convert("MCMXCIX"), Is.EqualTo("1999"));
         }
 
         [Test]
         public void Test2008()
         {
-            Assert.That("2008", Is.EqualTo(RomanConverter.Convert("MMVIII")));
+            Assert.That(RomanConverter.Convert("MMVIII"), Is.EqualTo("2008"));
         }
 
         [Test]
         public void Test19()
         {
-            Assert.That("19", Is.EqualTo(RomanConverter.Convert("XIX")));
+            Assert.That(RomanConverter.Convert("XIX"), Is.EqualTo("19"));
         }
     }
 }
